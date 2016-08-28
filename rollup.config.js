da224c728db8e84077ed47998c12c11ded97be3c
Dany Shaanan <danyshaanan@gmail.com>
2016-08-28 11:37:46 +0300

    Move Babel config into Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel'
 export default {
   entry: 'src/main.js',
   moduleName: 'mdaExample',
-  plugins: [babel()],
+  plugins: [babel({ presets: 'es2015-rollup' })],
   targets: [
     { dest: 'dist/bundle.amd.js', format: 'amd' },
     { dest: 'dist/bundle.cjs.js', format: 'cjs' },
