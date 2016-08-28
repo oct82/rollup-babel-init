dab5faba88bc4e3b44c1250632b86081a8c1076f
Dany Shaanan <danyshaanan@gmail.com>
2016-08-28 11:16:45 +0300

    Add output files in all formats

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,13 @@ import babel from 'rollup-plugin-babel'
 
 export default {
   entry: 'src/main.js',
-  format: 'umd',
-  moduleName: 'main',
+  moduleName: 'mdaExample',
   plugins: [babel()],
-  dest: 'main.bundle.js'
+  targets: [
+    { dest: 'dist/bundle.amd.js', format: 'amd' },
+    { dest: 'dist/bundle.cjs.js', format: 'cjs' },
+    { dest: 'dist/bundle.es.js', format: 'es' },
+    { dest: 'dist/bundle.iife.js', format: 'iife' },
+    { dest: 'dist/bundle.umd.js', format: 'umd' }
+  ]
 }
